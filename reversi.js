9e6891fd5569c0ce06c4797992b91fcc05ef8bd6
Kanishk Gupta <35873683+kgupta1542@users.noreply.github.com>
2020-07-27 22:07:23 -0700

    Update reversi.js

--- reversi.js
+++ reversi.js
@@ -41,6 +41,7 @@ function resetBoard(){
 	blackPieces = [];
 	
 	clearPotentialMoves();
+	moves = 0;
 	initGame();
 	turn.parentElement.style.display = "block";
 	endGameLabel.style.display = "none";
@@ -518,7 +519,7 @@ function gameTurn(type){
 	}
 	
 	if(type == "B" && oppPlayerType.innerHTML == "vs AI"){
-		aiTurn();
+		setTimeout(aiTurn(),2500);
 	}
 	else{
 		board.addEventListener('mousedown', addPiece, false);
